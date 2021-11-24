5687eeec79b96b415a4799808962b2455baa4b92
SimonTsankov <simoncankov@gmail.com>
2021-11-24 08:40:20 +0200

    Neon button added

--- Diplomna Rabota/client/src/app/app.component.ts
+++ Diplomna Rabota/client/src/app/app.component.ts
@@ -13,30 +13,72 @@ export class AppComponent implements OnInit {
   title = 'Rockmend me';
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
-  component: string="";
-
-  constructor(private router: Router,private observer: BreakpointObserver) {
+  component: string = "";
+  // @ts-ignore
+   bMobile = navigator.userAgent.indexOf( "Mobile" ) !== -1 ||
+    navigator.userAgent.indexOf( "iPhone" ) !== -1 ||
+    navigator.userAgent.indexOf( "Android" ) !== -1 ||
+    navigator.userAgent.indexOf( "Windows Phone" ) !== -1 ;
+  constructor(private router: Router, private observer: BreakpointObserver) {
     console.log("Yoohoo")
   }
 
 
   ngOnInit(): void {
-
+    this.randomNum()
     this.observer
       .observe(['(max-width: 800px)'])
       .pipe(delay(1))
       .subscribe((res) => {
-          this.sidenav.open();
+        this.sidenav.open();
       });
+    let element = document.getElementById("neon-btn")
+    if(this.bMobile){
+      // @ts-ignore
+      element.classList.add("neon-button-small")
+    }
+  }
+
+  async randomNum() {
+
+    let element = document.getElementById("neon-btn");
+
+
+    const mouseoverEvent = new Event('mouseover');
+    // @ts-ignore
+    element.dispatchEvent(mouseoverEvent);
+    // @ts-ignore
+    setTimeout(() => element.animate(
+      [
+        {
+
+          color: '#2b0229',
+          boxShadow: '0 0 1em 0.5em var(--neon-clr',
+          textShadow: 'none',
+          "background": "hsl(317, 100%, 54%)"//hsl(31, 100%, 50%) orange
+        }
+
+      ],
+      {duration: 1000, iterations: Math.random() * 2, easing: "ease-out"}
+      )
+      , 1000)
+
+    console.log(element)
+
+    setTimeout(() => {
+      this.randomNum()
+    }, Math.random() * 10000)
+
   }
 
   clickTest() {
     console.log("clicked!")
     this.router.navigate(["/login"]);
+
   }
 
   changePage(route: string) {
-    this.router.navigate(["/"+route]);
+    this.router.navigate(["/" + route]);
   }
 
 }
